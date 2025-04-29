4f7ed805b21f187c8216934b2f87a621987e550e
dougiemac (NixOS) <douglas.e.mckinley@gmail.com>
2025-04-29 16:16:29 -0700

    fix(home-manager): update flake.nix to support latest API changes
    - Replaced deprecated top-level arguments (`system`, `username`, etc.) in homeManagerConfiguration
    - Moved user settings into a module block
    - Ensured compatibility with Home Manager ≥ 22.11

--- flake.nix
+++ flake.nix
@@ -10,17 +10,18 @@
   outputs = { nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
-      username = "metalarms";
     in {
       homeConfigurations.metalarms = home-manager.lib.homeManagerConfiguration {
-        inherit system;
+        modules = [
+          ./home/metalarms.nix
+          {
+            home.username = "metalarms";
+            home.homeDirectory = "/home/metalarms";
+            home.stateVersion = "23.11"; # Match your current system or config
+          }
+        ];
         pkgs = nixpkgs.legacyPackages.${system};
-        modules = [ ./home/metalarms.nix ];
-        # optional, if you're not setting it in home.nix:
-        # username = "metalarms";
-        # homeDirectory = "/home/metalarms";
       };
-
 
       apps.${system}.update = {
         type = "app";
